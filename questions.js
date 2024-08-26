67ba6e39f616fdbdff4d6c5ef308139c9ff84d67
peculiar <pecuiheubammadu1st@gmail.com>
2024-08-26 12:16:45 +0100

    Commit

--- questions.js
+++ questions.js
@@ -1,54 +1,54 @@
-// const questions = [  
-//     { question: "1.) How do you write 'Hello World' in an alert box?",    
-//       options: ["A.) msgBox('Hello World')", "B.) alertBox('Hello World')", "C.) alert('Hello World')", "D.) msg('Hello World')"],
-//       answer: "2"
-//     },
-//     {
-//       question: "2.) Inside which HTML element do we put the JavaScript?",
-//       options: ["A.) script tag", "B.) javascript tag", "C.) js tag", "D.) scripting tag"],
-//       answer: "0"
-//     },
-//     {
-//       question: "3.) How do you create a function in JavaScript?",
-//       options: ["A.) function(myFunction)()", "B.) function = myFunction()", "C.) function:myFunction()", "D.) function myFunction()"],
-//       answer: "3"
-//     },
-//     {
-//       question: "4.) How do you call a function named 'myFunction'?",
-//       options: ["A.) call myFunction()", "B.) call function myFunction()", "C.) myFunction()", "D.) myFunction(function)"],
-//       answer: "2"
-//     },
-//     {
-//       question: "5.) Which event occurs when the user clicks on an HTML element?",
-//       options: ["A.) onmouseover", "B.) onclick", "C.) onchange", "D.) onmouseclick"],
-//       answer: "1"
-//     },
-//     {
-//       question: "6.) How to write an IF statement in JavaScript?",
-//       options: ["A.) if (i == 5)", "B.) if i = 5", "C.) if i == 5 then", "D.) if i = 5 then"],
-//       answer: "0"
-//     },
-//     {
-//       question: "7.) How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
-//       options: ["A.) if (i <> 5)", "B.) if i =! 5 then", "C.) if i <> 5", "D.) if (i != 5)"],
-//       answer: "3"
-//     },
-//     {
-//       question: "8.) How does a FOR loop start?",
-//       options: ["A.) for (i = 0; i <= 5; i++)", "B.) for i = 1 to 5", "C.) for (i <= 5; i++)", "D.) for (i = 0; i <= 5)"],
-//       answer: "0"
-//     },
-//     {
-//       question: "9.) How can you add a comment in a JavaScript?",
-//       options: ["A.) !--This is a comment--", "B.) 'This is a comment", "C.) This is a comment\\", "D.) //This is a comment"],
-//       answer: "3"
-//     },
-//     {
-//       question: "10.) How to insert a comment that has more than one line?",
-//       options: ["A.) !--This comment has more than one line--", "B.) /*This comment has more than one line*/", "C.) /This comment has more than one line/", "D.) \\This comment has more than one line\\"],
-//       answer: "1"
-//     }
-//   ];
+const questions = [  
+    { question: "1.) How do you write 'Hello World' in an alert box?",    
+      options: ["A.) msgBox('Hello World')", "B.) alertBox('Hello World')", "C.) alert('Hello World')", "D.) msg('Hello World')"],
+      answer: "2"
+    },
+    {
+      question: "2.) Inside which HTML element do we put the JavaScript?",
+      options: ["A.) script tag", "B.) javascript tag", "C.) js tag", "D.) scripting tag"],
+      answer: "0"
+    },
+    {
+      question: "3.) How do you create a function in JavaScript?",
+      options: ["A.) function(myFunction)()", "B.) function = myFunction()", "C.) function:myFunction()", "D.) function myFunction()"],
+      answer: "3"
+    },
+    {
+      question: "4.) How do you call a function named 'myFunction'?",
+      options: ["A.) call myFunction()", "B.) call function myFunction()", "C.) myFunction()", "D.) myFunction(function)"],
+      answer: "2"
+    },
+    {
+      question: "5.) Which event occurs when the user clicks on an HTML element?",
+      options: ["A.) onmouseover", "B.) onclick", "C.) onchange", "D.) onmouseclick"],
+      answer: "1"
+    },
+    {
+      question: "6.) How to write an IF statement in JavaScript?",
+      options: ["A.) if (i == 5)", "B.) if i = 5", "C.) if i == 5 then", "D.) if i = 5 then"],
+      answer: "0"
+    },
+    {
+      question: "7.) How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+      options: ["A.) if (i <> 5)", "B.) if i =! 5 then", "C.) if i <> 5", "D.) if (i != 5)"],
+      answer: "3"
+    },
+    {
+      question: "8.) How does a FOR loop start?",
+      options: ["A.) for (i = 0; i <= 5; i++)", "B.) for i = 1 to 5", "C.) for (i <= 5; i++)", "D.) for (i = 0; i <= 5)"],
+      answer: "0"
+    },
+    {
+      question: "9.) How can you add a comment in a JavaScript?",
+      options: ["A.) !--This is a comment--", "B.) 'This is a comment", "C.) This is a comment\\", "D.) //This is a comment"],
+      answer: "3"
+    },
+    {
+      question: "10.) How to insert a comment that has more than one line?",
+      options: ["A.) !--This comment has more than one line--", "B.) /*This comment has more than one line*/", "C.) /This comment has more than one line/", "D.) \\This comment has more than one line\\"],
+      answer: "1"
+    }
+  ];
 
-//   export default questions;
+  export default questions;
   
